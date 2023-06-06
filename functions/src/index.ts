b0837cb12750da9102b4ee45ac208782ef2dd2b0
Cartago <cartago.relaxingcup@gmail.com>
2023-06-07 00:05:41 +0200

    Importamos express y lo hacemos funcional con firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,8 @@
 import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import * as serviceAccount from "./private/serviceAccountKey.json";
+import * as express from "express";
+import * as cors from "cors";
 
 // import * as logger from "firebase-functions/logger";
 
@@ -38,9 +40,22 @@ export const getParam = onRequest((request, response) => {
 	});
 });
 
-export const getGoty = onRequest(async (_request, response) => {
+// export const getGoty = onRequest(async (_request, response) => {
+// 	const gotyRef = db.collection("goty");
+// 	const docsSnap = await gotyRef.get();
+// 	const juegos = docsSnap.docs.map((doc) => doc.data());
+// 	response.json(juegos);
+// });
+
+const app = express.default();
+app.use(cors.default({ origin: true }));
+
+// exports.api = onRequest(app);
+app.get("/get-goty", async (_request, response) => {
 	const gotyRef = db.collection("goty");
 	const docsSnap = await gotyRef.get();
 	const juegos = docsSnap.docs.map((doc) => doc.data());
 	response.json(juegos);
 });
+
+export const api = onRequest(app);
